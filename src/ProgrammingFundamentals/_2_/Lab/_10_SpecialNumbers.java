6fd7e68482f556c667b003188b0082e38f8f1eb4
Mario Petrov <maripetrov33@gmail.com>
2023-10-07 21:25:01 +0300

    Added new files

--- src/ProgrammingFundamentals/_2_/Lab/_10_SpecialNumbers.java
+++ src/ProgrammingFundamentals/_2_/Lab/_10_SpecialNumbers.java
@@ -1,4 +1,32 @@
 package ProgrammingFundamentals._2_.Lab;
 
+import java.util.Scanner;
+
 public class _10_SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=Integer.parseInt(sc.nextLine());
+
+        for(int i=1; i<=num; i++ ){
+
+            int current=i;
+            int sum=0;
+
+            while(current!=0){
+                sum+=current%10;
+                current=current/10;
+            }
+
+            if(sum==5){
+                System.out.printf("%d -> True\n",i);
+            } else if (sum==7) {
+                System.out.printf("%d -> True\n",i);
+            }else if (sum==11) {
+                System.out.printf("%d -> True\n",i);
+            }else {
+                System.out.printf("%d -> False\n",i);
+            }
+        }
+
+    }
 }
--- src/ProgrammingFundamentals/_2_/Lab/_5_ConcatNames.java
+++ src/ProgrammingFundamentals/_2_/Lab/_5_ConcatNames.java
@@ -1,4 +1,20 @@
 package ProgrammingFundamentals._2_.Lab;
 
+import java.util.Scanner;
+
 public class _5_ConcatNames {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        String delimeter = sc.nextLine();
+
+
+        StringBuilder result=new StringBuilder();
+        result.append(firstName)
+                .append(delimeter)
+                .append(lastName);
+
+        System.out.println(result);
+    }
 }
