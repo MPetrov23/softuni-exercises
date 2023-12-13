3848a9235ab17c37f5cb4ece5fe61ca1aa3a4428
Mario Petrov <maripetrov33@gmail.com>
2023-12-13 12:52:17 +0200

    Add files

--- src/groomingSalon/GroomingSalon.java
+++ src/groomingSalon/GroomingSalon.java
@@ -5,12 +5,60 @@ import java.util.List;
 
 public class GroomingSalon {
     private int capacity;
-    List<Pet> pets;
+    private List<Pet> data;
 
     public GroomingSalon(int capacity) {
         this.capacity = capacity;
-        pets = new ArrayList<>();
+        this.data = new ArrayList<>();
     }
 
-    
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void add(Pet pet){
+        if (data.size() < capacity) {
+            data.add(pet);
+            System.out.println("Pet added to the salon: " + pet);
+        } else {
+            System.out.println("Grooming salon is at full capacity. Cannot add more pets.");
+        }
+    }
+    public boolean remove(String name){
+        for(Pet pet : data){
+            if(pet.getName().equals(name)){
+                data.remove(pet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Pet getPet(String name, String owner){
+        for(Pet pet : data){
+            if(pet.getName().equals(name) && pet.getOwner().equals(owner)){
+                data.remove(pet);
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public int getCount(){
+        return data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder statistics = new StringBuilder("The grooming salon has the following clients:\n");
+        for (Pet pet : data) {
+            statistics.append(pet.getName()).append(" ").append(pet.getOwner()).append("\n");
+        }
+        return statistics.toString();
+    }
+
+
 }
--- src/groomingSalon/Pet.java
+++ src/groomingSalon/Pet.java
@@ -34,4 +34,9 @@ public class Pet {
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
+    @Override
+    public String toString(){
+        return String.format("%s %d - (%s)",name, age, owner);
+    }
 }
