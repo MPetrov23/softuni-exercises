00d1e4f81100fc83dea4902d53cb702a0367ad27
MPetrov23 <maripetrov33@gmail.com>
2024-03-13 21:40:09 +0200

    Upload files

---
+++ src/JavaAdvanced/OOP/Encapsulation/_4_FirstAndReserveTeam/Main.java
@@ -0,0 +1,27 @@
+package JavaAdvanced.OOP.Encapsulation._4_FirstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        Team team = new Team("Black Eagles");
+
+        List<Person> players = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = sc.nextLine().split("\\s+");
+            players.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
+        }
+
+        for (Person player : players) {
+            team.addPlayer(player);
+        }
+
+        System.out.printf("First team have %d players%n", team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players%n", team.getReserveTeam().size());
+    }
+}
