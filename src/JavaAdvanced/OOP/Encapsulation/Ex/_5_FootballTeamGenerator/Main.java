4a1963e76355852f244bc1d9abcc9ee548f8be5b
MPetrov23 <maripetrov33@gmail.com>
2024-03-15 20:27:02 +0200

    Update file

--- src/JavaAdvanced/OOP/Encapsulation/Ex/_5_FootballTeamGenerator/Main.java
+++ src/JavaAdvanced/OOP/Encapsulation/Ex/_5_FootballTeamGenerator/Main.java
@@ -7,51 +7,74 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         List<Team> teams = new ArrayList<>();
         String command = sc.nextLine();
 
         while (!command.equals("END")) {
             String[] commandParts = command.split(";");
+            String action = commandParts[0];
+            String teamName = commandParts[1];
 
-            if (commandParts[0].equals("Team")) {
-                Team team = new Team(commandParts[1]);
-                teams.add(team);
-            } else if (commandParts[0].equals("Add")) {
-                boolean isTeamExist = false;
-                for (Team team : teams) {
-                    if (team.getName().equals(commandParts[1])) {
-                        isTeamExist = true;
-                        try {
-                            Player player = new Player(commandParts[2], Integer.parseInt(commandParts[3]), Integer.parseInt(commandParts[4]),
-                                    Integer.parseInt(commandParts[5]), Integer.parseInt(commandParts[6]) ,Integer.parseInt(commandParts[7]));
-                            team.addPlayer(player);
-                        } catch (IllegalArgumentException exception) {
-                            System.out.printf("%s%n",exception.getMessage());
-                        }
-                    }
-                }
-                if (!isTeamExist) {
-                    System.out.printf("Team %s does not exist.%n", commandParts[1]);
-                }
-            } else if (commandParts[0].equals("Remove")) {
-                for (Team team : teams) {
-                    if (team.getName().equals(commandParts[1])) {
-                        team.removePlayer(commandParts[2]);
-                    }
-                }
-            } else if (commandParts[0].equals("Rating")) {
-                boolean isTeamExist = false;
-                for (Team team : teams) {
-                    if (team.getName().equals(commandParts[1])) {
-                        isTeamExist = true;
-                        System.out.printf("%s - %.0f%n", team.getName(), team.getRating());
-                    }
-                }
-                if (!isTeamExist) {
-                    System.out.printf("Team %s does not exist.%n", commandParts[1]);
-                }
+            if (action.equals("Team")) {
+                    createTeam(teams,teamName);
+            } else if (action.equals("Add")) {
+                    addPlayer(teams,teamName,commandParts);
+            } else if (action.equals("Remove")) {
+                    removePlayer(teams,teamName,commandParts[2]);
+            } else if (action.equals("Rating")) {
+                    getTeamRating(teams,teamName);
             }
             command = sc.nextLine();
         }
+
     }
+
+    public static void createTeam(List<Team> teams, String teamName){
+        Team team = new Team(teamName);
+        teams.add(team);
+    }
+
+    public static void addPlayer(List<Team> teams,String teamName, String[] commandParts){
+        boolean isTeam = false;
+
+        for (Team team : teams) {
+            if (team.getName().equals(teamName)) {
+                isTeam = true;
+                try {
+                    Player player = new Player(commandParts[2], Integer.parseInt(commandParts[3]), Integer.parseInt(commandParts[4]),
+                            Integer.parseInt(commandParts[5]), Integer.parseInt(commandParts[6]) ,Integer.parseInt(commandParts[7]));
+                    team.addPlayer(player);
+                } catch (IllegalArgumentException e) {
+                    System.out.printf("%s%n",e.getMessage());
+                }
+            }
+        }
+        if (!isTeam) {
+            System.out.printf("Team %s does not exist.%n", teamName);
+        }
+
+    }
+
+    public static void removePlayer(List<Team> teams, String teamName, String player) {
+        for (Team team : teams) {
+            if (team.getName().equals(teamName)) {
+                team.removePlayer(player);
+            }
+        }
+    }
+
+    public static void getTeamRating(List<Team> teams, String teamName) {
+        boolean isTeamExist = false;
+        for (Team team : teams) {
+            if (team.getName().equals(teamName)) {
+                isTeamExist = true;
+                System.out.printf("%s - %.0f%n", team.getName(), team.getRating());
+            }
+        }
+        if (!isTeamExist) {
+            System.out.printf("Team %s does not exist.%n", teamName);
+        }
+    }
+
 }
