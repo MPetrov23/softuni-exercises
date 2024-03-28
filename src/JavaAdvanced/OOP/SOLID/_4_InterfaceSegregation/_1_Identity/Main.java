585d3bbe1fe958744b575b38cb209790357d8d0d
MPetrov23 <maripetrov33@gmail.com>
2024-03-28 18:30:18 +0200

    Add files

---
+++ src/JavaAdvanced/OOP/SOLID/_4_InterfaceSegregation/_1_Identity/Main.java
@@ -0,0 +1,7 @@
+package JavaAdvanced.OOP.SOLID._4_InterfaceSegregation._1_Identity;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/JavaAdvanced/OOP/SOLID/_4_InterfaceSegregation/_1_Identity/interfaces/User.java
@@ -0,0 +1,7 @@
+package JavaAdvanced.OOP.SOLID._4_InterfaceSegregation._1_Identity.interfaces;
+
+public interface User {
+    String getEmail();
+
+    String getPasswordHash();
+}
